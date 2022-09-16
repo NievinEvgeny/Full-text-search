d32fd6dceddae94abac936afd7fc22a62c32643d
NievinEvgeny <slip@Oldlord.localdomain>
2022-09-16 23:59:25 +0700

    Add test lib that sum 2 numbers

---
+++ external/ftslib/fts.cxx
@@ -0,0 +1,6 @@
+#include <fts.hpp>
+
+float sum(float a, float b)
+{
+    return a + b;
+}
---
+++ src/main.cxx
@@ -0,0 +1,11 @@
+#include <external/ftslib/fts.hpp>
+#include <iostream>
+
+int main()
+{
+    float a = 3, b = 4;
+    float res;
+    res = sum(a, b);
+    std::cout << res << "\n";
+    return 0;
+}
