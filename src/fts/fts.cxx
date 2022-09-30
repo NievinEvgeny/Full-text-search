b8feada392a80a942f131afdc2eb436691f8ce17
NievinEvgeny <zhenya-nievin@mail.ru>
2022-09-30 07:03:23 +0700

    Add exception check for min length of ngram

--- src/fts/fts.cxx
+++ src/fts/fts.cxx
@@ -14,9 +14,14 @@ void parse_config(
 {
     std::ifstream conf_file(config_filename);
     nlohmann::json parsed_config = nlohmann::json::parse(conf_file);
-    text = parsed_config.at("text");
+
     ngram_min_length = parsed_config.at("ngram_min_length");
+    if (ngram_min_length < 1)
+    {
+        throw "Ngram min length is below 1";
+    }
     ngram_max_length = parsed_config.at("ngram_max_length");
+    text = parsed_config.at("text");
     stop_words = parsed_config.at("stop_words");
 }
 
@@ -27,7 +32,14 @@ void run_parser(const std::string& config_filename)
     int ngram_min_length;
     int ngram_max_length;
 
-    parse_config(config_filename, text, stop_words, ngram_min_length, ngram_max_length);
+    try
+    {
+        parse_config(config_filename, text, stop_words, ngram_min_length, ngram_max_length);
+    }
+    catch (const char* exception)
+    {
+        std::cerr << exception << '\n';
+    }
 }
 
 }  // namespace fts
