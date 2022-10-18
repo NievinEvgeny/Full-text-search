045a0fde7ff6db36720a204e0ccbd0d57ebeea7d
NievinEvgeny <zhenya-nievin@mail.ru>
2022-10-19 04:10:02 +0700

    Delete/improve/fix some tests

--- tests/fts_parser.cxx
+++ tests/fts_parser.cxx
@@ -1,70 +1,7 @@
 #include <fts/fts.hpp>
 #include <gtest/gtest.h>
 #include <string>
-
-TEST(char_to_lower_case, simple)
-{
-    std::string expect = "dr. jekyll and mr. hyde";
-    std::string real = "Dr. Jekyll and Mr. Hyde";
-    fts::char_to_lower_case(real);
-    EXPECT_TRUE(expect == real);
-}
-
-TEST(char_to_lower_case, no_capital_letters)
-{
-    std::string expect = "dr. jekyll and mr. hyde";
-    std::string real = "dr. jekyll and mr. hyde";
-    fts::char_to_lower_case(real);
-    EXPECT_TRUE(expect == real);
-}
-
-TEST(char_to_lower_case, empty_string)
-{
-    std::string expect = "";
-    std::string real = "";
-    fts::char_to_lower_case(real);
-    EXPECT_TRUE(expect == real);
-}
-
-TEST(remove_punctuation, simple)
-{
-    std::string expect = "Dr Jekyll and Mr Hyde";
-    std::string real = "Dr. Jekyll and Mr. Hyde";
-    fts::remove_punctuation(real);
-    EXPECT_TRUE(expect == real);
-}
-
-TEST(remove_punctuation, no_punct)
-{
-    std::string expect = "Dr Jekyll and Mr Hyde";
-    std::string real = "Dr Jekyll and Mr Hyde";
-    fts::remove_punctuation(real);
-    EXPECT_TRUE(expect == real);
-}
-
-TEST(remove_punctuation, empty_string)
-{
-    std::string expect = "";
-    std::string real = "";
-    fts::remove_punctuation(real);
-    EXPECT_TRUE(expect == real);
-}
-
-TEST(remove_punctuation, do_not_ruin_text)
-{
-    std::string expect = "ABCDE";
-    std::string real = ".A,B.C,D.E,.,.,.,.,,";
-    fts::remove_punctuation(real);
-    EXPECT_TRUE(expect == real);
-}
-
-TEST(remove_punctuation, only_punct_in_text)
-{
-    std::string expect = "";
-    std::string real = ".,.,.,.,.,,";
-    fts::remove_punctuation(real);
-    EXPECT_TRUE(expect == real);
-}
+#include <unordered_set>
 
 TEST(string_tokenization, simple)
 {
@@ -79,18 +16,16 @@ TEST(string_tokenization, simple)
 
 TEST(string_tokenization, white_spaces)
 {
-    std::vector<std::string> expect;
     std::string text = "   ";
     std::vector<std::string> real = fts::string_tokenization(text);
-    EXPECT_TRUE(expect.size() == real.size());
+    EXPECT_TRUE(real.empty());
 }
 
 TEST(string_tokenization, empty)
 {
-    std::vector<std::string> expect;
     std::string text = "";
     std::vector<std::string> real = fts::string_tokenization(text);
-    EXPECT_TRUE(expect.size() == real.size());
+    EXPECT_TRUE(real.empty());
 }
 
 TEST(string_tokenization, white_spaces_everywhere)
@@ -106,7 +41,7 @@ TEST(string_tokenization, white_spaces_everywhere)
 
 TEST(delete_stop_words, simple)
 {
-    std::vector<std::string> stop_words{"dr", "and", "mr"};
+    const std::unordered_set<std::string> stop_words{"dr", "and", "mr"};
     std::vector<std::string> expect{"jekyll", "hyde"};
     std::vector<std::string> real{"dr", "jekyll", "and", "mr", "hyde"};
     fts::delete_stop_words(real, stop_words);
@@ -118,16 +53,15 @@ TEST(delete_stop_words, simple)
 
 TEST(delete_stop_words, no_relevant_words)
 {
-    std::vector<std::string> stop_words{"dr", "and", "mr", "a", "with"};
-    std::vector<std::string> expect;
+    const std::unordered_set<std::string> stop_words{"dr", "and", "mr", "a", "with"};
     std::vector<std::string> real{"dr", "and", "mr", "a", "with"};
     fts::delete_stop_words(real, stop_words);
-    EXPECT_TRUE(expect.size() == real.size());
+    EXPECT_TRUE(real.empty());
 }
 
 TEST(delete_stop_words, numbers_in_stop_words)
 {
-    std::vector<std::string> stop_words{"dr", "and", "mr", "a12", "with"};
+    const std::unordered_set<std::string> stop_words{"dr", "and", "mr", "a12", "with"};
     std::vector<std::string> expect;
     std::vector<std::string> real{"dr", "and", "mr", "a12", "with"};
     fts::delete_stop_words(real, stop_words);
@@ -139,19 +73,7 @@ TEST(ngram_generation, simple)
     int ngram_min_length = 3, ngram_max_length = 6;
     std::vector<std::string> text_tokens{"jekyll", "hyde"};
 
-    std::vector<fts::ngram> expect;
-    fts::ngram temp = {0, "jek"};
-    expect.push_back(temp);
-    temp = {0, "jeky"};
-    expect.push_back(temp);
-    temp = {0, "jekyl"};
-    expect.push_back(temp);
-    temp = {0, "jekyll"};
-    expect.push_back(temp);
-    temp = {1, "hyd"};
-    expect.push_back(temp);
-    temp = {1, "hyde"};
-    expect.push_back(temp);
+    std::vector<fts::ngram> expect = {{0, "jek"}, {0, "jeky"}, {0, "jekyl"}, {0, "jekyll"}, {1, "hyd"}, {1, "hyde"}};
     std::vector<fts::ngram> real;
 
     real = fts::ngram_generation(text_tokens, ngram_min_length, ngram_max_length);
@@ -184,23 +106,12 @@ TEST(ngram_generation, big_and_small_words)
     text_tokens.push_back("peepoclown");
     text_tokens.push_back("hy");
 
-    std::vector<fts::ngram> expect;
-    fts::ngram temp = {0, "pee"};
-    expect.push_back(temp);
-    temp = {0, "peep"};
-    expect.push_back(temp);
-    temp = {0, "peepo"};
-    expect.push_back(temp);
-    temp = {0, "peepoc"};
-    expect.push_back(temp);
-
+    std::vector<fts::ngram> expect = {{0, "pee"}, {0, "peep"}, {0, "peepo"}, {0, "peepoc"}};
     std::vector<fts::ngram> real;
 
     real = fts::ngram_generation(text_tokens, ngram_min_length, ngram_max_length);
     for (int i = 0; i < 4; i++)
     {
-        std::cout << expect[i].word << '\n';
-        std::cout << real[i].word << '\n';
         EXPECT_TRUE(expect[i].index == real[i].index);
         EXPECT_TRUE(expect[i].word == real[i].word);
     }
