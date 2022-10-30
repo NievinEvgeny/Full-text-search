8fe7870fb7bc5e5c49222354326f8e94dadc6d26
NievinEvgeny <zhenya-nievin@mail.ru>
2022-10-31 00:02:29 +0700

    Add separate utilities for indexation and search + some options

--- src/main.cxx
+++ src/main.cxx
@@ -8,32 +8,60 @@ int main(int argc, char** argv)
 {
     cxxopts::Options options("TextSearcher");
 
-    options.add_options()("config,config_file", "Config file name", cxxopts::value<std::string>());
-
-    const auto parse_cmd_line = options.parse(argc, argv);
-
-    std::string conf_filename = "RunOptions.json";
-
-    if (parse_cmd_line.count("config") == 1)
-    {
-        conf_filename = parse_cmd_line["config"].as<std::string>();
-    }
+    // clang-format off
+    options.add_options()
+        ("i,indexer", "Indexer call")
+        ("s,searcher", "Searcher call")
+        ("index,index_path", "Path for index", cxxopts::value<std::string>()->default_value("index"))
+        ("q,query", "Query to search", cxxopts::value<std::string>())
+        ("c,config_file", "Config file name", cxxopts::value<std::string>()->default_value("RunOptions.json"))
+        ("h,help", "Print usage");
+    // clang-format on
 
     try
     {
-        fts::ConfOptions conf_options = fts::parse_config(conf_filename);
+        const auto parse_cmd_line = options.parse(argc, argv);
 
-        fts::IndexBuilder indexes;
-        indexes.add_document(103975, "The Matrix Matrix Matrix Reloaded Revolution", conf_options);  // delete
-        indexes.add_document(238695, "The Matrix Revolution", conf_options);  // delete
-        indexes.add_document(390473, "The Matrix", conf_options);  // delete
+        if (parse_cmd_line.count("help"))
+        {
+            std::cout << options.help() << '\n';
+            return 0;
+        }
 
-        fts::TextIndexWriter index_writer("index");
-        index_writer.write(indexes.get_index());
+        std::string index_path = parse_cmd_line["index_path"].as<std::string>();
+
+        if (parse_cmd_line.count("indexer"))
+        {
+            std::string conf_filename = parse_cmd_line["config_file"].as<std::string>();
+
+            fts::ConfOptions conf_options = fts::parse_config(conf_filename);
+
+            fts::IndexBuilder indexes;
+            indexes.add_document(103975, "The Matrix Matrix Matrix Reloaded Revolution", conf_options);  // delete
+            indexes.add_document(238695, "The Matrix Revolution", conf_options);  // delete
+            indexes.add_document(390473, "The Matrix", conf_options);  // delete
+
+            fts::TextIndexWriter index_writer(index_path);
+            index_writer.write(indexes.get_index());
+            // TODO (create folder if index_path isn't default)
+            // TODO (add config copy to serialization)
+        }
+
+        if (parse_cmd_line.count("searcher"))
+        {
+            // TODO (implementation of searcher)
+        }
     }
+
+    catch (const cxxopts::exceptions::exception& msg)
+    {
+        std::cerr << msg.what() << '\n';
+        return -1;
+    }
+
     catch (const std::exception& msg)
     {
-        std::cout << msg.what() << '\n';
+        std::cerr << msg.what() << '\n';
         return -1;
     }
 
