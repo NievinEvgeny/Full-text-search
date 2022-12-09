1196b000f6598a543613f5984cc6dba2dab79979
NievinEvgeny <zhenya-nievin@mail.ru>
2022-12-10 05:14:39 +0700

    Fix and merge tests in fts_searcher

--- tests/fts_searcher.cxx
+++ tests/fts_searcher.cxx
@@ -8,49 +8,7 @@
 #include <string>
 #include <vector>
 
-TEST(IndexAccessor, check_scores)
-{
-    const std::string conf_filename = "../../../RunOptions.json";
-    const fts::ConfOptions config = fts::parse_config(conf_filename);
-
-    fts::IndexBuilder indexes{config};
-
-    indexes.add_document(1, "The Matrix");
-    indexes.add_document(2, "The Clown");
-    indexes.add_document(3, "The PoroSad");
-
-    const std::string temp_dir = std::filesystem::temp_directory_path().string();
-    const std::filesystem::path index_docs(temp_dir + "/docs");
-    const std::filesystem::path index_entries(temp_dir + "/entries");
-
-    const std::vector<std::filesystem::path> directories = {index_docs, index_entries};
-
-    for (const auto& dir : directories)
-    {
-        if (!std::filesystem::exists(dir))
-        {
-            std::filesystem::create_directory(dir);
-        }
-    }
-
-    fts::TextIndexWriter index_writer(temp_dir);
-    index_writer.write(indexes.get_index());
-
-    const std::string query = "Matrix";
-    const std::vector<fts::Ngram> ngrams = fts::parse_query(config, query);
-
-    fts::IndexAccessor index_accessor(temp_dir, ngrams);
-    const std::vector<fts::DocScore> doc_scores = index_accessor.get_scores();
-
-    std::filesystem::remove_all(index_docs);
-    std::filesystem::remove_all(index_entries);
-
-    ASSERT_NEAR(doc_scores.at(0).score, 4.39444, 0.0001);
-    ASSERT_DOUBLE_EQ(doc_scores.at(1).score, 0);
-    ASSERT_DOUBLE_EQ(doc_scores.at(2).score, 0);
-}
-
-TEST(IndexAccessor, score_sort_stability)
+TEST(IndexAccessor, score_check_and_stability)
 {
     const std::string conf_filename = "../../../RunOptions.json";
     const fts::ConfOptions config = fts::parse_config(conf_filename);
@@ -59,6 +17,7 @@ TEST(IndexAccessor, score_sort_stability)
 
     indexes.add_document(1, "The Matrix");
     indexes.add_document(2, "The Matrix");
+    indexes.add_document(3, "The Clown");
 
     const std::string temp_dir = std::filesystem::temp_directory_path().string();
     const std::filesystem::path index_docs(temp_dir + "/docs");
@@ -74,18 +33,19 @@ TEST(IndexAccessor, score_sort_stability)
         }
     }
 
-    fts::TextIndexWriter index_writer(temp_dir);
+    fts::TextIndexWriter index_writer(temp_dir, config);
     index_writer.write(indexes.get_index());
 
     const std::string query = "Matrix";
-    const std::vector<fts::Ngram> ngrams = fts::parse_query(config, query);
 
-    fts::IndexAccessor index_accessor(temp_dir, ngrams);
-    const std::vector<fts::DocScore> doc_scores = index_accessor.get_scores();
+    fts::TextIndexAccessor index_accessor(temp_dir, config);
+    fts::Searcher searcher(index_accessor);
+    const fts::SearchInfo search_result = searcher.score_calc(query);
 
     std::filesystem::remove_all(index_docs);
     std::filesystem::remove_all(index_entries);
 
-    EXPECT_TRUE(doc_scores.at(0).score == doc_scores.at(1).score);
-    EXPECT_TRUE(doc_scores.at(0).doc_id < doc_scores.at(1).doc_id);
+    ASSERT_NEAR(search_result.docs_scores.at(0).score, 1.6218, 0.0001);
+    EXPECT_TRUE(search_result.docs_scores.at(0).score == search_result.docs_scores.at(1).score);
+    EXPECT_TRUE(search_result.docs_scores.at(0).doc_id < search_result.docs_scores.at(1).doc_id);
 }
